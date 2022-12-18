8bb187360c39eee7b629dffa3f534aee2232b2ed
allmightdude <sabzi_f@yahoo.com>
2022-12-18 18:12:10 +0330

    Create Product Model

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+    categoryID : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Category"
+    },
+    ownerID : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Owner"
+    },
+    title : String,
+    description : String,
+    photo : String,
+    price : Number,
+    stockQuantity : Number,
+    reviews : [{    
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Review"
+    }]
+} , {
+    toObject : { virtuals : true },
+    toJSON : { virtuals : true }
+})
+
+module.exports = mongoose.model('Product' , ProductSchema)
