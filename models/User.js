9d4d6ad3ab2acb77ab1deabf9e83c7ba0d99c7a4
allmightdude <sabzi_f@yahoo.com>
2022-12-18 18:03:17 +0330

    User Model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Address",
+  },
+});
+
+
+module.exports = mongoose.model('User' , UserSchema);
--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('morgan');
-const connectDB = require('./config/db');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("morgan");
+const connectDB = require("./config/db");
+const path = require("path");
+const dotenv = require("dotenv");
 
-dotenv.config({path : './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended : false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/' , (req , res) => {
-    res.send('hi')
-})
+app.get("/", (req, res) => {
+    res.json(`askdlasdk`);
+
+});
 
 connectDB();
 
 const PORT = process.env.PORT;
-app.listen(3000 , () => console.log(`Server Runing on Port : ${PORT}`))
+app.listen(3000, () => console.log(`Server Runing on Port : ${PORT}`));
