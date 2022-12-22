f6272e82271303ab96ec6d96c3e64d1f909fbc2c
allmightdude <sabzi_f@yahoo.com>
2022-12-22 13:13:34 +0330

    Create Owners API

--- routes/owner.js
+++ routes/owner.js
@@ -1,42 +1,41 @@
-const router = require('express').Router();
-const Owner = require('../models/Owner');
+const router = require("express").Router();
+const Owner = require("../models/Owner");
 
 // post owner
-router.post('/owner', async (req , res)=>{
-    try {
-        let owner = new Owner();
-        owner.name = req.body.name;
-        owner.about = req.body.about;
-        owner.photo = `result.secure_url`;
+router.post("/owner", async (req, res) => {
+  try {
+    let owner = new Owner();
+    owner.name = req.body.name;
+    owner.about = req.body.about;
+    owner.photo = `result.secure_url`;
 
-        await owner.save();
-        res.json({
-            succuss : true,
-            msg :'Success Create New Owner!'
-        })
-    } 
-    catch (error) {
-        res.status(500).json({
-            success : false ,
-            msg : error.message
-        })
-    }
-})
+    await owner.save();
+    res.json({
+      succuss: true,
+      msg: "Success Create New Owner!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      msg: error.message,
+    });
+  }
+});
 
 // get owner
-router.get('/owner' , async (req , res) => {
-    try {
-        let owners = await Owner.find();
+router.get("/owner", async (req, res) => {
+  try {
+    let owners = await Owner.find();
+    res.json({
+      success: true,
+      owners: owners,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      meesage: "nothing owner ...",
+    });
+  }
+});
 
-        res.json({
-            success: true , 
-            owners : owners
-        })
-    }catch (error) {
-        res.status(500).json({
-            success: false , 
-            meesage : "nothing owner ..."
-        })
-    }
-})
 module.exports = router;
