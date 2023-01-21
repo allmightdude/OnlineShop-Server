5adbc778f01e79dbb9bbc54e8b4577a3b5fdb38d
allmightdude <sabzi_f@yahoo.com>
2023-01-21 19:52:20 +0330

    Payment Api - Post a order

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const moment = require("moment");
-const Stripe = require("stripe");
+const Stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const Order = require("../models/Order");
 const verifyToken = require("../middlewares/verify-token");
 
@@ -43,37 +43,52 @@ router.post("/shipment", (req, res) => {
   }
 });
 
-router.post('/payment' , verifyToken , (req , res) => {
+router.post("/payment", verifyToken, (req, res) => {
   let totalPrice = Math.round(req.body.totalPrice * 100);
 
   Stripe.customers
     .create({
-      email : req.decoded.email 
+      email: req.decoded.email,
     })
-    .then(customer => {
-      return Stripe.customers.createSource(customer.id , {
-        source : "tok_visa"
-      })
+    .then((customer) => {
+      return Stripe.customers.createSource(customer.id, {
+        source: "tok_visa",
+      });
     })
-    .then(source => {
+    .then((source) => {
       return Stripe.charges.create({
-        amount : totalPrice ,
-        currency : "usd",
-        customer : source.customer
-      })
+        amount: totalPrice,
+        currency: "usd",
+        customer: source.customer,
+      });
     })
-    .then(async charge => {
+    .then(async (charge) => {
       let order = new Order();
       let cart = req.body.cart;
 
-      cart.map(product => {
+      cart.map((product) => {
         order.products.push({
-          productID : product._id,
-          quantity : parseInt(product.quantity),
-          price : product.price
-        })
+          productID: product._id,
+          quantity: parseInt(product.quantity),
+          price: product.price,
+        });
+      });
+
+      order.owner = req.decoded._id;
+      order.estimatedDelivery = req.body.estimatedDelivery;
+      await order.save();
+
+      res.json({
+        success : true,
+        msg : "Succussfully create order!"
       })
-    })
-})
+    });
+  //   .catch((err) => {
+  //     res.status(500).json({
+  //       success: true,
+  //       msg: err.message,
+  //     });
+  //   });
+});
 
 module.exports = router;
