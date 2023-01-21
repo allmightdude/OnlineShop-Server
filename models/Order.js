e2f800f2db5c5485a3902cf61627410fdd04849d
allmightdude <sabzi_f@yahoo.com>
2023-01-21 18:22:52 +0330

    Order Schema

--- models/Order.js
+++ models/Order.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-   type :{
-    type : String,
-    unique : true,
-    required : true
-   }
-})
+  products: [
+    {
+      productID: { type: mongoose.Schema.Types.ObjectId },
+      quantity: Number,
+      price: Number,
+    },
+  ],
+  estimatedDelivery: String,
+});
 
-module.exports = mongoose.model('Order' , OrderSchema)
+module.exports = mongoose.model("Order", OrderSchema);
