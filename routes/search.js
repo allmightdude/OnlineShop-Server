c5e1088288c14b8ad594f527068ceee912ec8da1
allmightdude <sabzi_f@yahoo.com>
2023-01-24 16:03:15 +0330

    Search API

--- routes/search.js
+++ routes/search.js
@@ -6,15 +6,16 @@ router.get("/search", async (req, res) => {
   try {
     const filters = req.query;
     const Products = await Product.find();
-
+    console.log(req.query);
     const filterdProducts = Products.filter((prd) => {
       let isValid = true;
       for (key in filters) {
-        isValid = isValid && prd[key] === filters[key];
+        isValid = isValid && prd[key] == filters[key];
       }
       return isValid;
     });
     res.json({
+      success: true,
       filterdProducts: filterdProducts,
     });
   } catch (error) {
